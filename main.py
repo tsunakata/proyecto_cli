a9245875bfda78e947d75d2625e4c19e2440b9c8
tsunakata <tsunakata@gmail.com>
2023-04-17 22:10:03 -0500

    primer commit

---
+++ main.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+current_path = Path.cwd()
+print(current_path)
+
+while True:
+    print("Menú: ")
+    print("1) Listar documentos.")
+    print("2) Leer documento.")
+    print("3) Eliminar documento.")
+    print("4) Salir.")
+
+    choice = int(input("Escribe una opción: "))
+
+    if choice == 1:
+        print("Listar documento")
+    elif choice == 2:
+        print("Leer documento")
+    elif choice == 3:
+        print("Eliminar documento")
+    elif choice == 4:
+        break
+    else:
+        print("Opción incorrecta.")
